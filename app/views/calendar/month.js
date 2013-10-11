8d7071c6f721eb54dc7c134a1b035fe00a22927d
Mikhail Baranov <mikhail.baranov@gmail.com>
2013-10-12 02:31:26 +0400

    Добавление событий по клику на ячейку календаря

--- app/views/calendar/month.js
+++ app/views/calendar/month.js
@@ -11,6 +11,21 @@ define([
         initialize: function(options) {
             this.year = options.year;
             this.month = options.month;
+
+            this.eventsCollection = options.eventsCollection;
+        },
+
+        events: {
+            'click .day': 'addEvent'
+        },
+
+        addEvent: function(e) {
+            var $day = $(e.target);
+
+            this.eventsCollection.add({
+                date: $day.data('date'),
+                title: $.now()
+            });
         },
 
         render: function() {
--- app/views/index.js
+++ app/views/index.js
@@ -2,14 +2,19 @@ define([
     'jquery',
     'underscore',
     'backbone',
+    'collections/events',
     'views/header',
     'views/calendar/month',
     'jade!templates/layout','jade!templates/layout',
     'styl!/s/styles'
-], function ($, _, Backbone, HeaderView, MonthView, layoutTemplate) {
+], function ($, _, Backbone, EventsCollection, HeaderView, MonthView, layoutTemplate) {
 
     return Backbone.View.extend({
         el: '.app',
+
+        initialize: function() {
+            this.eventsCollection = new EventsCollection();
+        },
 
         render: function() {
             var headerView;
@@ -19,7 +24,11 @@ define([
 
             headerView = new HeaderView();
             headerView.render();
-            monthView = new MonthView({year: 2013, month: 9});
+            monthView = new MonthView({
+                year: 2013,
+                month: 9,
+                eventsCollection: this.eventsCollection
+            });
             monthView.render();
 
             return this;
