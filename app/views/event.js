97403134d87d9f880f1b6107626601acd833b4b1
Mikhail Baranov <mikhail.baranov@gmail.com>
2013-10-12 22:33:57 +0400

    Написал вью и шаблон отдельного события

---
+++ app/views/event.js
@@ -0,0 +1,48 @@
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'jade!templates/event'
+], function ($, _, Backbone, template) {
+    return Backbone.View.extend({
+        tagName: "article",
+        className: "event",
+
+        initialize: function(params) {
+            this.options = params;
+
+            this.model && this.model.on('destroy', this.remove, this);
+        },
+
+        events: {
+            'click .closer': 'closerHandler',
+            'click .eventTitle': 'titleHandler'
+        },
+
+        closerHandler: function() {
+            if (this.options.canDestroy) {
+                this.destroy();
+            }
+        },
+
+        titleHandler: function() {
+            if (this.options.canExpand) {
+                this.expand();
+            }
+        },
+
+        destroy: function() {
+                this.model.destroy();
+        },
+
+        expand: function() {
+            this.$('.eventText').toggleClass('.eventText-expanded');
+        },
+
+        render: function() {
+            var templateData = _.extend({}, this.model.toJSON(), this.options);
+
+            this.$el.html(template(this.model.toJSON(templateData)));
+        }
+    });
+});
