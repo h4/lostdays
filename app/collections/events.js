92b6653e5ab46bb77e771d39db440a92f7e4a445
Mikhail Baranov <mikhail.baranov@gmail.com>
2013-10-12 04:29:10 +0400

    Сохранение моделей в localStorage

--- app/collections/events.js
+++ app/collections/events.js
@@ -5,6 +5,27 @@ define([
     'models/event'
 ], function ($, _, Backbone, EventModel) {
     return Backbone.Collection.extend({
-        model: EventModel
+        model: EventModel,
+
+        saveAll: function() {
+            this.sync('update', this);
+        },
+
+        sync: function(method, collection) {
+            if (method == 'read') {
+                if ('localStorage' in window) {
+                    var items = localStorage.getItem('events');
+
+                    if (items) {
+                        collection.set(JSON.parse(items));
+                    }
+                }
+            }
+            if (method == 'create' || 'update') {
+                if ('localStorage' in window) {
+                    localStorage.setItem('events', JSON.stringify(collection.toJSON()));
+                }
+            }
+        }
     });
 });
