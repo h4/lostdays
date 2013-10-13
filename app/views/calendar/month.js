0ef62df80b74bc2fde00c165c12ae3e7c08f4244
Mikhail Baranov <mikhail.baranov@gmail.com>
2013-10-13 16:46:37 +0400

    При добавлении события показываем его в календаре

--- app/views/calendar/month.js
+++ app/views/calendar/month.js
@@ -16,6 +16,8 @@ define([
             this.month = options.month;
 
             this.currentEvents = this.collection.getByMonth(this.year, this.month);
+
+            this.collection.on('add', this.addNewEvent, this);
         },
 
         events: {
@@ -42,27 +44,27 @@ define([
             }
         },
 
-        showEvents: function() {
-            var eventView;
+        addExistedEvents: function() {
+            this.currentEvents.forEach(_.bind(this.addNewEvent));
+        },
 
-            this.currentEvents.forEach(_.bind(function(elem) {
-                eventView = new EventView({
-                    canDestroy: false,
-                    canExpand: false,
-                    model: elem
-                }).render().$el.appendTo(this.$('.day[data-date="' + elem.get('date') + '"]').find('.events'));
-            }));
+        addNewEvent: function(model) {
+            new EventView({
+                canDestroy: false,
+                canExpand: false,
+                model: model
+            }).render().$el.appendTo(this.$('.day[data-date="' + model.get('date') + '"]').find('.events'));
         },
 
         render: function() {
-            htmlData = {
+            var htmlData = {
                 days: monthGenerator.getMonth(this.year, this.month),
                 month: this.month,
                 year: this.year
             };
 
             this.$el.html(template(htmlData));
-            this.showEvents();
+            this.addExistedEvents();
 
             return this;
         }
